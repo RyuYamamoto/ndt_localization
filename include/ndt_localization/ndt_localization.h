d76d059ba766975d1ca1f5241dee0dd69a5d0d18
RyuYamamoto <ryu.yamamoto@tier4.jp>
2022-10-27 11:27:28 +0900

    feat: port humble

--- include/ndt_localization/ndt_localization.h
+++ include/ndt_localization/ndt_localization.h
@@ -7,10 +7,17 @@
 #include <std_msgs/msg/float32.hpp>
 
 #include <tf2/transform_datatypes.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <tf2_ros/transform_broadcaster.h>
 #include <tf2_ros/transform_listener.h>
 #include <tf2_sensor_msgs/tf2_sensor_msgs.h>
+
+#ifdef USE_TF2_GEOMETRY_MSGS_DEPRECATED_HEADER
+#include <tf2_eigen/tf2_eigen.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#else
+#include <tf2_eigen/tf2_eigen.hpp>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#endif
 
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/io/io.h>
