a00a1a5ca2dc0d9e248e7e9227e737f18b70a031
RyuYamamoto <rottertarmenation.y11.r18@gmail.com>
2026-05-22 21:45:58 +0900

    support jazzy
    Signed-off-by: RyuYamamoto <rottertarmenation.y11.r18@gmail.com>

--- include/ndt_localization/ndt_localization.h
+++ include/ndt_localization/ndt_localization.h
@@ -24,7 +24,7 @@
 #include <tf2/transform_datatypes.h>
 #include <tf2_ros/transform_broadcaster.h>
 #include <tf2_ros/transform_listener.h>
-#include <tf2_sensor_msgs/tf2_sensor_msgs.h>
+#include <tf2_sensor_msgs/tf2_sensor_msgs.hpp>
 
 class NDTLocalization : public rclcpp::Node
 {
